e9abda9990ee61351688a1d26d946d3d8db535db
estefanoo <estefanocarlos54@gmail.com>
2020-10-09 20:11:53 -0300

    Variable de entorno MONGOO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV === 'dev'){
 
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = process.env.MONGO_URL
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
